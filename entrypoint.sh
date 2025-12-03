85fd07fac4bb59e363d7a0d624617e90932f0d89
Luis Felipe de la hoz <lfdelahozfontalvo@gmail.com>
2025-12-03 10:11:34 -0500

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@ set -e
 
 echo "Iniciando contenedor..."
 
-# Asegurar que el puerto de Railway esté disponible
 PORT=${PORT:-5000}
 
 echo "Levantando servidor con Gunicorn en el puerto $PORT"
 
-exec gunicorn app:app \
+exec gunicorn main:app \
     --bind 0.0.0.0:$PORT \
     --workers 3 \
-    --worker-class gevent \
+    --worker-class gthread \
+    --threads 4 \
     --timeout 120
